1684b1d7e6872fb64312adcd61fbbfb8435bfb9e
yaki koren <yaki.koren@gmail.com>
2019-11-21 12:32:28 +0200

    renamed folders

--- src/03 - controller/controller.js
+++
@@ -1,9 +0,0 @@
-/*export*/ var myapp;
-(function (myapp) {
-    function myController($scope) {
-        $scope.firstName = "John";
-        $scope.lastName = "Doe";
-    }
-    myapp.myController = myController;
-    angular.module('myApp', []).controller('myCtrl', myController);
-})(myapp || (myapp = {}));
